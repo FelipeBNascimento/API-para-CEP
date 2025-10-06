63cef430cf4f26656cdee9ecd76d978ad44252bb
Bazan <felipe_bazan3006@hotmail.com>
2025-10-06 10:25:22 -0300

    Aplicando a classe security

--- src/main/java/com/cepbazan/apicep/infraesctruture/security/CepSecurity.java
+++ src/main/java/com/cepbazan/apicep/infraesctruture/security/CepSecurity.java
@@ -1,4 +1,31 @@
 package com.cepbazan.apicep.infraesctruture.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
 public class CepSecurity {
+
+    // Configuração do filtro de segurança
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Desativa proteção CSRF para APIs REST (não aplicável a APIs que não mantêm estado)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/cep").authenticated()// Não Permite acesso ao endpoint GET sem autenticação
+                        .requestMatchers(HttpMethod.DELETE, "/cep").authenticated() // Não Permite acesso ao endpoint DELETE sem autenticação
+                        .requestMatchers(HttpMethod.POST, "/cep").permitAll() // Permite acesso ao endpoint POST /usuario sem autenticação
+                        .anyRequest().permitAll() // Permite para todas as outras requisições
+                )
+                .httpBasic(httpBasic -> {}); // Habilita autenticação Http Bascic, pedindo usuario e senha no cabeçalho Authorization
+
+
+        // Retorna a configuração do filtro de segurança construída
+        return http.build();
+    }
+
 }
